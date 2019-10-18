9707b4f24f35565fb327036bc69444dd357239ce
Perttu Lähteenlahti <perttu@Perttu-MacBook-Pro.local>
2019-10-18 10:09:36 +0300

    Fix to graphql query

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -26,21 +26,6 @@ const About = (props: Props) => {
 					title
 				}
 			}
-			allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
-				edges {
-					node {
-						excerpt
-						fields {
-							slug
-						}
-						frontmatter {
-							date(formatString: "MMMM DD, YYYY")
-							title
-							description
-						}
-					}
-				}
-			}
 		}
 	`);
 
