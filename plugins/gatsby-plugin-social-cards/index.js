63f3e095d30dd4a41b77929bc916c47452199d98
Perttu Lähteenlahti <perttu.lahteenlahti@gmail.com>
2019-11-06 20:53:34 +0200

    Testing

--- plugins/gatsby-plugin-social-cards/index.js
+++ plugins/gatsby-plugin-social-cards/index.js
@@ -7,8 +7,8 @@ module.exports = ({ markdownNode }) => {
 
 	return Promise.all([
 		jimp.read(path.join(__dirname, 'base.jpg')),
-		jimp.loadFont(jimp.FONT_SANS_32_BLACK)
+		jimp.loadFont(jimp.FONT_SANS_128_BLACK)
 	]).then(([image, font]) => {
-		image.print(font, 50, 50, frontmatter.title, 300).write(output);
+		image.print(font, 50, 100, frontmatter.title, 1100).write(output);
 	});
 };
