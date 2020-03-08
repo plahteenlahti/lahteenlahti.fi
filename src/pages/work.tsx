00e588809d9e31fb996406c8653d1d25ae56b657
Perttu Lähteenlahti <perttu.lahteenlahti@gmail.com>
2020-03-08 15:09:09 +0200

    Add titles

--- src/pages/work.tsx
+++ src/pages/work.tsx
@@ -1,46 +1,39 @@
-import { graphql, PageRendererProps, useStaticQuery } from 'gatsby';
-import React from 'react';
-import styled from 'styled-components';
-import { Bio } from '../components/bio';
-import { Layout } from '../components/layout';
-import { FadeLink } from '../components/link';
-import { SEO } from '../components/seo';
-import { MarkdownRemark } from '../graphql-types';
-import { rhythm } from '../utils/typography';
-
-const StyledLink = styled(FadeLink)`
-	box-shadow: none;
-`;
-
-const Title = styled.h3`
-	margin-bottom: ${rhythm(1 / 4)};
-`;
+import { graphql, PageRendererProps, useStaticQuery } from "gatsby";
+import React from "react";
+import { Layout } from "../components/layout";
+import { SEO } from "../components/seo";
 
 type Props = PageRendererProps;
 
 const About = (props: Props) => {
-	const data = useStaticQuery(graphql`
-		query {
-			site {
-				siteMetadata {
-					title
-				}
-			}
-		}
-	`);
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `);
 
-	const siteTitle = data.site.siteMetadata.title;
+  const siteTitle = data.site.siteMetadata.title;
 
-	return (
-		<Layout location={props.location} title={siteTitle}>
-			<SEO
-				slug='work'
-				title='Work by Perttu Lähteenlahti'
-				keywords={[`work`, `portfolio`, `javascript`, `react`]}
-			/>
-			<h1>What I've worked on</h1>
-		</Layout>
-	);
+  return (
+    <Layout location={props.location} title={siteTitle}>
+      <SEO
+        slug="work"
+        title="Work by Perttu Lähteenlahti"
+        keywords={[`work`, `portfolio`, `javascript`, `react`]}
+      />
+      <h1>What I've worked on</h1>
+
+      <h3>Nyxo</h3>
+
+      <h3>Nordea Analysis 360°</h3>
+
+      <h3>Scudo Lite & Scudo Pro</h3>
+    </Layout>
+  );
 };
 
 export default About;
