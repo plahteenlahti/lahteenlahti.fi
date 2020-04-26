796843d734f13276c782abbae63264382e31f906
Perttu Lähteenlahti <perttu.lahteenlahti@gmail.com>
2020-04-26 15:28:11 +0300

    Better replace

--- plugins/gatsby-plugin-social-cards/index.js
+++ plugins/gatsby-plugin-social-cards/index.js
@@ -13,10 +13,9 @@ module.exports = ({ markdownNode }) => {
     jimp.loadFont(path.join(__dirname, "Montserrat.fnt")),
   ]).then(([image, font]) => {
     const title = frontmatter.title.replace(
-      /([\uE000-\uF8FF]|\uD83C[\uDF00-\uDFFF]|\uD83D[\uDC00-\uDDFF])/g,
+      /([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g,
       ""
     );
-
     image.print(font, 50, 120, title, 1100).write(output);
   });
 };
