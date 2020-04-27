f55a394e7803e1950a71cf835aa6165738e640a9
Perttu Lähteenlahti <perttu.lahteenlahti@gmail.com>
2020-04-27 12:52:59 +0300

    The cards look really good now

--- plugins/gatsby-plugin-social-cards/index.js
+++ plugins/gatsby-plugin-social-cards/index.js
@@ -1,21 +1,45 @@
 const path = require("path");
 const jimp = require("jimp");
 
-module.exports = ({ markdownNode }) => {
-  const { frontmatter, fields, fileAbsolutePath } = markdownNode;
-  const slug = fileAbsolutePath.includes("weeklies")
-    ? `/weekly${fields.slug}`
-    : fields.slug;
+module.exports = async ({ markdownNode }) => {
+  const { frontmatter, fields, fileAbsolutePath, timeToRead } = markdownNode;
+  const isWeekly = fileAbsolutePath.includes("weeklies");
+  const slug = isWeekly ? `/weekly${fields.slug}` : fields.slug;
   const output = path.join("./public", slug, "seo.jpg");
+  const hasTags =
+    frontmatter.tags && frontmatter.tags.length !== 0
+      ? frontmatter.tags[0]
+      : "Article";
+  const tag = isWeekly ? "Weekly" : hasTags;
+  const time = timeToRead ? `${timeToRead}m` : "3m";
 
-  return Promise.all([
-    jimp.read(path.join(__dirname, "base.jpg")),
-    jimp.loadFont(path.join(__dirname, "Montserrat.fnt")),
-  ]).then(([image, font]) => {
-    const title = frontmatter.title.replace(
-      /([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g,
-      ""
-    );
-    image.print(font, 50, 120, title, 1100).write(output);
-  });
+  const titleFont = await jimp.loadFont(path.join(__dirname, "Title.fnt"));
+  const detailFont = await jimp.loadFont(
+    path.join(__dirname, "Montserrat-Medium.ttf.fnt")
+  );
+
+  return Promise.all([jimp.read(path.join(__dirname, "template.png"))]).then(
+    ([image]) => {
+      const title = frontmatter.title.replace(
+        /([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g,
+        ""
+      );
+      image.print(detailFont, 132, 341, time, 1100).write(output);
+      image.print(detailFont, 315, 341, tag, 1100).write(output);
+      image
+        .print(
+          titleFont,
+          77,
+          10,
+          {
+            text: title,
+            alignmentX: jimp.HORIZONTAL_ALIGN_LEFT,
+            alignmentY: jimp.VERTICAL_ALIGN_BOTTOM,
+          },
+          700,
+          300
+        )
+        .write(output);
+    }
+  );
 };
