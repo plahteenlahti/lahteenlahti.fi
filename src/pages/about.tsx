e1e7c8749063ce766892c99d5d4da1ddf4cef159
Perttu <perttu.lahteenlahti@gmail.com>
2022-12-28 12:30:03 +0200

    update(about): update text

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -27,12 +27,7 @@ const About = (props: Props) => {
       />
       <h1>Who is Perttu Lähteenlahti?</h1>
       <div style={{ paddingBottom: 100 }}>
-        Here's a little backstory of the person your website you're viewing: I'm
-        a developer &amp; designer. Currently, I'm doing that in a company I
-        started called <a href='https://nyxo.fi'>Nyxo</a>, our main product is a
-        mobile app providing personalized sleep coaching. This is my third
-        company so far and the first one that can be considered to be a startup.
-        The two previous ones were software consultancies.
+      I am a developer and designer who founded Nyxo, a startup that offers personalized sleep coaching through a mobile app. Prior to starting Nyxo, I worked as a product lead at the University of Helsinki and as an analyst at Nordea, a Nordic bank. Before that, I paid my way through university by fixing iPhones and working as a research assistant, while also studying cognitive science and computer science at the University of Helsinki.
         <br /> <br />
         In addition to these three companies, I've worked in a variety of
         different companies and projects. Before Nyxo I worked as a product lead
@@ -40,65 +35,13 @@ const About = (props: Props) => {
         that I was an analyst at Nordea, a big Nordic bank, building investment
         solutions for SMEs.
         <br /> <br />
-        However, before all this I also paid my way through university by fixing
-        iPhones, working as a research assistant. I've studied both cognitive
-        science and computer science at the University of Helsinki. I was a
-        decent student, but even better at studying fast. I did five years worth
-        of studies in two years before the working life got to me. For that
-        reason, I'm still considered a student as I've never finished my
-        master's thesis.
+        In addition to my work as a developer, I have written for several publications, including Muzli, DailyJS, codeburst, and Hackernoon. My writings often cover topics related to my personal experiences. I like to talk and write about topics such as:
         <br /> <br />
-        Originally I was born in Pori, a small town on the west coast of
-        Finland. My family was poor, my mom being a welder and my father being a
-        carpenter. Partly due to my father's occupation I have worked as a
-        carpenter myself as well, for five years. Four of those years I spent
-        working for my father, in his company, which eventually went bankrupt
-        and bankrupted both of my parent's as well.
-        <br /> <br /> Coming from a poor family, I have worked my hardest to
-        become as financially independent as possible. This has partly been the
-        catalyst for my interest in investing and building companies. I am by no
-        means rich, but throughout the years I've accumulated enough wealth to
-        buy apartments for both my parent's and little sister, as well as pursue
-        my dreams of University education.
-        <br /> <br />
-        This information should give you a good view of what has shaped my world
-        view. What it does not necessarily state is what my writings tend to be
-        about, so I'm going to end this about section by mentioning those. Most
-        of my writings can be found from this blog, but I've also written for:
-        <a href='https://medium.muz.li/'>Muzli</a>,
-        <a href='https://medium.com/dailyjs'>DailyJS</a>,
-        <a href='https://codeburst.io/'>codeburst</a>, and{" "}
-        <a href='https://hackernoon.com/'>Hackernoon</a>. I like talking and
-        writing about:
-        <ul>
-          <li>Technology</li>
-          <li>
-            <Link to='tags/programming/'>Programming</Link> (currently that
-            means React and React Native mostly)
-          </li>
-          <li>
-            <Link to='tags/design/'>Design</Link>, of UIs and products
-          </li>
-          <li>Diversity, especially in technology</li>
-          <li>
-            Innovation (the fact that I've competed in over 70 different
-            innovation challenges provides me with a unique view on this)
-          </li>
-          <li>
-            <Link to='tags/building-companies/'>Building companies</Link>
-          </li>
-        </ul>
         You can reach me through email, [first name]@lahteenlahti.fi and from{" "}
         <a href='https://www.linkedin.com/in/plahteenlahti/'>LinkedIn</a> and{" "}
         <a href='https://twitter.com/plahteenlahti'>Twitter.</a>
         <br /> <br />
-        In addition to this blog, I also have{" "}
-        <a href='https://perttu.dev'>
-          separate one focusing on programming
-        </a>{" "}
-        and a side project one focusing on{" "}
-        <a href='https://react-native.club'>React Native development.</a>
-        <br></br>
+        <br /> <br />
         <em>Perttu Lähteenlahti</em>
       </div>
     </Layout>
